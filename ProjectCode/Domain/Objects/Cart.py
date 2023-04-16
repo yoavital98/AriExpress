f8076d8eb74ab1fdffc6bd686a345a0d1ddafa98
Ari <arioshryz@gmail.com>
2023-04-17 00:16:02 +0300

    a bit of signatures and admins #10

---
+++ ProjectCode/Domain/Objects/Cart.py
@@ -0,0 +1,3 @@
+class Cart:
+    def __init__(self, holder_id):
+        self.holder_id = holder_id
--- ProjectCode/Service/Service.py
+++ ProjectCode/Service/Service.py
@@ -1,110 +1,229 @@
 from ProjectCode.Domain.Controllers.StoreFacade import *
+import logging
 
 
-class Service:
-    def __init__(self):
-        self.store_facade = StoreFacade()  # activates loadData in constructor
+class Service:  # TODO change ValueError to relevant ones
+    def __init__(self, logging):
+        self.admins = self.loadAdminsFromDB()
+        self.store_facade = None # StoreFacade() TODO decide how we want to open teh facade
+
+
+
+
+    # ------  admin  ------ #
+    def openTheSystem(self, requesterID):
+        try:
+            if self.admins.keys().__contains__(requesterID):  # check if admin
+                self.store_facade = StoreFacade()  # activates loadData in constructor
+            else:
+                raise SystemError("open-the-system requester isn't admin")
+        except SystemError:
+            logging.getLogger().error(SystemError)
+
+    def addAdmin(self, requesterID, newAdminID, newPassword, newEmail):  # TODO we will assume the requesting admin fills himself the new admin details
+        try:
+            if self.admins.keys().__contains__(requesterID):  # check if admin
+                self.admins[newAdminID] = Admin(newAdminID, newPassword, newEmail)
+            raise SystemError("add-admin requester isn't admin")
+        except SystemError:
+            logging.getLogger().error(SystemError)
+
+    def loadAdminsFromDB(self):
+        try:
+            admins = TypedDict(string, Admin)
+            ### let's assume we tried to take all the admins from the database
+            if len(admins) == 0:#TODO: just for this version so we will have an Admin
+                admins["007"] = Admin("JamesBond", "TATAKAE", "yoavital98@gmail.com")
+            return admins
+        except ValueError:
+            pass
 
     # ------  users  ------ #
 
     #  Guests
-    def enterTheSystem(self):
-        pass
 
     def exitTheSystem(self):
-        pass
+        try:
+            self.store_facade.exitTheSystem()
+        except ValueError:
+            pass
 
     #  Members
-    def register(self):
-        pass
+    def register(self, user_name, password, email):
+        try:
+            self.store_facade.register()
+        except ValueError:
+            pass
 
-    def logIn(self):
-        pass
+    def logIn(self,username, password):
+        try:
+            self.store_facade.logIn()
+        except ValueError:
+            pass
 
-    def logOut(self):
-        pass
+    def logOut(self,username):
+        try:
+            self.store_facade.logOut()
+        except ValueError:
+            pass
 
-    def getMemberPurchaseHistory(self):
-        pass
+    def getMemberPurchaseHistory(self,username):
+        try:
+            self.store_facade.getMemberPurchaseHistory()
+        except ValueError:
+            pass
 
     # ------  stores  ------ #
 
     def getStores(self):
-        pass
+        try:
+            self.store_facade.getStores()
+        except ValueError:
+            pass
 
-    def getProductsByStore(self):
-        pass
+    def getProductsByStore(self, storename):
+        try:
+            self.store_facade.getProductsByStore()
+        except ValueError:
+            pass
 
-    def getProduct(self):
-        pass
+    def getProduct(self, storename, productName):
+        try:
+            self.store_facade.getProduct()
+        except ValueError:
+            pass
 
-    def productSearchByName(self):  # and keywords
-        pass
+    def productSearchByName(self, productName):  # and keywords
+        try:
+            self.store_facade.productSearchByName()
+        except ValueError:
+            pass
 
-    def productSearchByCategory(self):
-        pass
+    def productSearchByCategory(self, categoryName):#TODO: probably each store will have its products catagorized
+        try:#TODO: need to create an enum set of categories, shopowners does not create categories.!!!!!!!
+            self.store_facade.productSearchByCategory()
+        except ValueError:
+            pass
 
-    def productFilterByFeatures(self):
-        pass
+    def productFilterByFeatures(self):#TODO: need to think of a functionality
+        try:
+            self.store_facade.productFilterByFeatures()
+        except ValueError:
+            pass
 
-    def getBasket(self):
-        pass
+    def getBasket(self,username, storename):
+        try:
+            self.store_facade.getBasket()
+        except ValueError:
+            pass
 
-    def getCart(self):
-        pass
+    def getCart(self,username):
+        try:
+            self.store_facade.getCart()
+        except ValueError:
+            pass
 
-    def addToBasket(self):
-        pass
+    def addToBasket(self,username, storename, productname, quantity):
+        try:
+            self.store_facade.addToBasket()
+        except ValueError:
+            pass
 
-    def removeFromBasket(self):
-        pass
+    def removeFromBasket(self,username, storename, productname):
+        try:
+            self.store_facade.removeFromBasket()
+        except ValueError:
+            pass
 
-    def editBasketQuantity(self):
-        pass
+    def editBasketQuantity(self,username, storename, productname, quantity):
+        try:
+            self.store_facade.editBasketQuantity()
+        except ValueError:
+            pass
 
-    def purchaseCart(self):
-        pass
+    def purchaseCart(self, username, cardnumber, cardusername, carduserID, carddate, backnumber):#TODO: for now lets assume only credit card(no paypal)
+        try:
+            self.store_facade.purchaseCart()
+        except ValueError:
+            pass
 
-    def placeBid(self):
-        pass
+    def placeBid(self,username, storename, productname, bid):
+        try:
+            self.store_facade.placeBid()
+        except ValueError:
+            pass
 
-    def getStorePurchaseHistory(self):
-        pass
+    def getStorePurchaseHistory(self, username, storename):#TODO: username is demanded for validation of the request
+        try:
+            self.store_facade.getStorePurchaseHistory()
+        except ValueError:
+            pass
 
     # ------  Management  ------ #
 
-    def openStore(self):
-        pass
+    def openStore(self,username):
+        try:
+            self.store_facade.openStore()
+        except ValueError:
+            pass
 
-    def addNewProductToStore(self):
-        pass
+    def addNewProductToStore(self,username, storename , productname, productcategory, productquantity, productprice):
+        try:
+            self.store_facade.addNewProductToStore()
+        except ValueError:
+            pass
 
     def removeProductFromStore(self):
-        pass
+        try:
+            self.store_facade.removeProductFromStore()
+        except ValueError:
+            pass
 
     def editProductOfStore(self):
-        pass
+        try:
+            self.store_facade.editProductOfStore()
+        except ValueError:
+            pass
 
     def nominateStoreOwner(self):
-        pass
+        try:
+            self.store_facade.nominateStoreOwner()
+        except ValueError:
+            pass
 
     def nominateStoreManager(self):
-        pass
+        try:
+            self.store_facade.nominateStoreManager()
+        except ValueError:
+            pass
 
     def addPermissionsForManager(self):
-        pass
+        try:
+            self.store_facade.addPermissionsForManager()
+        except ValueError:
+            pass
 
     def editPermissionsForManager(self):
-        pass
+        try:
+            self.store_facade.editPermissionsForManager()
+        except ValueError:
+            pass
 
     def closeStore(self):
-        pass
+        try:
+            self.store_facade.closeStore()
+        except ValueError:
+            pass
 
     def getStaffInfo(self):
-        pass
+        try:
+            self.store_facade.getStaffInfo()
+        except ValueError:
+            pass
 
     def getStoreManagerPermissions(self):
-        pass
-
-
+        try:
+            self.store_facade.getStoreManagerPermissions()
+        except ValueError:
+            pass
 
