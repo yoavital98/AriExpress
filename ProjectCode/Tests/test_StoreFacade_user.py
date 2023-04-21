03cde28ed3f49ed43c80ba17f41e3d53e5b8b6f6
yoavital <yoavital98@gmail.com>
2023-04-21 14:09:41 +0300

    Edited implementations for tests

--- ProjectCode/Tests/test_StoreFacade_user.py
+++ ProjectCode/Tests/test_StoreFacade_user.py
@@ -11,10 +11,10 @@ class TestStoreFacade(TestCase):
     def setUp(self):
         # TODO: check info for duplicates
         self.store_facade = StoreFacade()
-        # self.member1 = Member("John", "password123", "john.doe@example.com")
-        # self.store_facade.members["John"] = self.member1
-        # self.member2 = Member("Jane", "password456", "jane.doe@example.com")
-        # self.store_facade.members["Jane"] = self.member2
+        self.member1 = Member("John", "password123", "john.doe@example.com")
+        self.store_facade.members["John"] = self.member1
+        self.member2 = Member("Jane", "password456", "jane.doe@example.com")
+        self.store_facade.members["Jane"] = self.member2
         # self.username = "test_user"
         # self.password = "test_password"
         # self.member = Member(self.username, self.password, "testing111@gmail.com")
@@ -30,42 +30,62 @@ class TestStoreFacade(TestCase):
     #     self.fail()
 
     def test_register_new_member_success(self):
+        # check a new register signup
         username = "test_user"
         password = "test_password"
         email = "test_email@example.com"
-        # self.store_facade.passwordValidator.ValidatePassword = MagicMock(return_value=True)
         member = self.store_facade.register(username, password, email)
         self.assertEqual(self.store_facade.members[username], member)
 
     def test_register_new_member_failure(self):
-        username = "test_user"
-        password = "test_password"
-        email = "test_email@example.com"
-        # self.store_facade.passwordValidator.ValidatePassword = MagicMock(return_value=True)
-        member = self.store_facade.register(username, password, email)
-        self.assertRaises(self.store_facade.register(username, password, email), SystemError)
+        # trying to register an existing user
+        self.assertRaises(self.store_facade.register("John", "password123", "john.doe@example.com"), SystemError)
 
-    def test_checkIfUserIsLoggedIn_existingUserLoggedIn(self):
+    def test_checkIfUserIsLoggedIn_existingUserLoggedIn_success(self):
         self.member1.logInAsMember()
         result = self.store_facade.checkIfUserIsLoggedIn("John")
         self.assertEqual(result, True)
 
-    def test_checkIfUserIsLoggedIn_existingUserNotLoggedIn(self):
+    def test_checkIfUserIsLoggedIn_existingUserLoggedIn_failure(self):
+        self.member1.logInAsMember()
+        result = self.store_facade.checkIfUserIsLoggedIn("John")
+        self.assertEqual(result, True)
+
+    def test_checkIfUserIsLoggedIn_existingUserNotLoggedIn_success(self):
+        self.member1.logOff()
+        result = self.store_facade.checkIfUserIsLoggedIn("John")
+        self.assertEqual(result, False)
+    def test_checkIfUserIsLoggedIn_existingUserNotLoggedIn_failure(self):
         self.member1.logOff()
         result = self.store_facade.checkIfUserIsLoggedIn("John")
         self.assertEqual(result, False)
 
-    def test_checkIfUserIsLoggedIn_nonExistingUser(self):
+    def test_checkIfUserIsLoggedIn_nonExistingUser_success(self):
+        with self.assertRaises(SystemError):
+            self.store_facade.checkIfUserIsLoggedIn("NonExistingUser")
+    def test_checkIfUserIsLoggedIn_nonExistingUser_failure(self):
         with self.assertRaises(SystemError):
             self.store_facade.checkIfUserIsLoggedIn("NonExistingUser")
 
-    def test_logInAsGuest(self):
+    def test_logInAsGuest_success(self):
+        entrance_id = self.store_facade.nextEntranceID
+        guest = self.store_facade.logInAsGuest()
+        self.assertEqual(guest.id, entrance_id)
+        self.assertIn(str(entrance_id), self.store_facade.onlineGuests)
+    def test_logInAsGuest_failure(self):
         entrance_id = self.store_facade.nextEntranceID
         guest = self.store_facade.logInAsGuest()
         self.assertEqual(guest.id, entrance_id)
         self.assertIn(str(entrance_id), self.store_facade.onlineGuests)
 
-    def test_leaveAsGuest(self):
+    def test_leaveAsGuest_success(self):
+        entrance_id = self.store_facade.nextEntranceID
+        guest = Mock()
+        guest.id = entrance_id
+        self.store_facade.onlineGuests[str(entrance_id)] = guest
+        self.store_facade.leaveAsGuest(entrance_id)
+        self.assertNotIn(str(entrance_id), self.store_facade.onlineGuests)
+    def test_leaveAsGuest_failure(self):
         entrance_id = self.store_facade.nextEntranceID
         guest = Mock()
         guest.id = entrance_id
@@ -73,7 +93,16 @@ class TestStoreFacade(TestCase):
         self.store_facade.leaveAsGuest(entrance_id)
         self.assertNotIn(str(entrance_id), self.store_facade.onlineGuests)
 
-    def test_logInAsMember_existing_user(self):
+    def test_logInAsMember_existing_user_success(self):
+        # add member to members dictionary
+        self.store_facade.members[self.username] = self.member
+
+        # call login function
+        logged_in_member = self.store_facade.logInAsMember(self.username, self.password)
+
+        # check that the member is logged in
+        self.assertTrue(logged_in_member.get_logged())
+    def test_logInAsMember_existing_user_failure(self):
         # add member to members dictionary
         self.store_facade.members[self.username] = self.member
 
@@ -83,12 +112,16 @@ class TestStoreFacade(TestCase):
         # check that the member is logged in
         self.assertTrue(logged_in_member.get_logged())
 
-    def test_logInAsMember_non_existing_user(self):
+    def test_logInAsMember_non_existing_user_success(self):
+        # call login function for non-existing user
+        with self.assertRaises(SystemError):
+            self.store_facade.logInAsMember("non_existing_user", self.password)
+    def test_logInAsMember_non_existing_user_failure(self):
         # call login function for non-existing user
         with self.assertRaises(SystemError):
             self.store_facade.logInAsMember("non_existing_user", self.password)
 
-    def test_logInAsMember_wrong_password(self):
+    def test_logInAsMember_wrong_password_success(self):
         # add member to members dictionary
         self.store_facade.members[self.username] = self.member
 
@@ -96,7 +129,23 @@ class TestStoreFacade(TestCase):
         with self.assertRaises(SystemError):
             self.store_facade.logInAsMember(self.username, "wrong_password")
 
-    def test_logOut_member_exists(self):
+    def test_logInAsMember_wrong_password_failure(self):
+        # add member to members dictionary
+        self.store_facade.members[self.username] = self.member
+
+        # call login function with wrong password
+        with self.assertRaises(SystemError):
+            self.store_facade.logInAsMember(self.username, "wrong_password")
+
+    def test_logOut_member_exists_success(self):
+        username = "test_user111"
+        password = "password123"
+        member = Member(username, password, "testingEmail@gmail.com")
+        self.store_facade.members[username] = member
+        member.logInAsMember()
+        self.store_facade.logOut(username)
+        self.assertFalse(member.get_logged())
+    def test_logOut_member_exists_failure(self):
         username = "test_user111"
         password = "password123"
         member = Member(username, password, "testingEmail@gmail.com")
@@ -105,13 +154,28 @@ class TestStoreFacade(TestCase):
         self.store_facade.logOut(username)
         self.assertFalse(member.get_logged())
 
-    def test_logOut_member_does_not_exist(self):
+    def test_logOut_member_does_not_exist_success(self):
+        # Try to log out a member that does not exist
+        username = "nonexistent_user"
+        self.store_facade.logOut(username)
+        self.assertTrue(True)
+    def test_logOut_member_does_not_exist_failure(self):
         # Try to log out a member that does not exist
         username = "nonexistent_user"
         self.store_facade.logOut(username)
         self.assertTrue(True)
 
-    def test_getMemberPurchaseHistory_returns_transaction_history_for_valid_username(self):
+    def test_getMemberPurchaseHistory_returns_transaction_history_for_valid_username_success(self):
+        # Arrange
+        test_username = "test_user"
+        # TODO: create a mock TransactionHistory and add some transactions for the test user
+
+        # Act
+        result = self.store_facade.getMemberPurchaseHistory(test_username)
+
+        # Assert
+        # TODO: add assertions to verify that the correct TransactionHistory is returned
+    def test_getMemberPurchaseHistory_returns_transaction_history_for_valid_username_failure(self):
         # Arrange
         test_username = "test_user"
         # TODO: create a mock TransactionHistory and add some transactions for the test user
@@ -122,7 +186,16 @@ class TestStoreFacade(TestCase):
         # Assert
         # TODO: add assertions to verify that the correct TransactionHistory is returned
 
-    def test_getMemberPurchaseHistory_returns_none_for_invalid_username(self):
+    def test_getMemberPurchaseHistory_returns_none_for_invalid_username_success(self):
+        # Arrange
+        test_username = "invalid_user"
+
+        # Act
+        result = self.store_facade.getMemberPurchaseHistory(test_username)
+
+        # Assert
+        self.assertIsNone(result)
+    def test_getMemberPurchaseHistory_returns_none_for_invalid_username_failure(self):
         # Arrange
         test_username = "invalid_user"
 
@@ -132,7 +205,15 @@ class TestStoreFacade(TestCase):
         # Assert
         self.assertIsNone(result)
 
-    def test_getMemberPurchaseHistory_raises_error_if_user_is_not_logged_in(self):
+    def test_getMemberPurchaseHistory_raises_error_if_user_is_not_logged_in_success(self):
+        # Arrange
+        test_username = "test_user"
+        # TODO: log the test user out
+
+        # Act & Assert
+        with self.assertRaises(Exception):
+            self.store_facade.getMemberPurchaseHistory(test_username)
+    def test_getMemberPurchaseHistory_raises_error_if_user_is_not_logged_in_failure(self):
         # Arrange
         test_username = "test_user"
         # TODO: log the test user out
@@ -141,7 +222,14 @@ class TestStoreFacade(TestCase):
         with self.assertRaises(Exception):
             self.store_facade.getMemberPurchaseHistory(test_username)
 
-    def test_get_basket(self):
+    def test_get_basket_success(self):
+        # test with invalid username, should return None
+        self.assertIsNone(self.store_facade.getBasket("invalid_username", "Store1"))
+
+        # test with valid username and storename, should return a Cart object
+        basket = self.store_facade.getBasket("JohnDoe", "Store1")
+        self.assertIsInstance(basket, Cart)
+    def test_get_basket_failure(self):
         # test with invalid username, should return None
         self.assertIsNone(self.store_facade.getBasket("invalid_username", "Store1"))
 
@@ -149,13 +237,24 @@ class TestStoreFacade(TestCase):
         basket = self.store_facade.getBasket("JohnDoe", "Store1")
         self.assertIsInstance(basket, Cart)
 
-    def test_get_basket_with_invalid_storename(self):
+    def test_get_basket_with_invalid_storename_success(self):
+        # test with invalid storename, should raise an error
+        with self.assertRaises(SystemError):
+            self.store_facade.getBasket("JohnDoe", "invalid_storename")
+    def test_get_basket_with_invalid_storename_failure(self):
         # test with invalid storename, should raise an error
         with self.assertRaises(SystemError):
             self.store_facade.getBasket("JohnDoe", "invalid_storename")
 
     # (self,username):
-    def test_get_cart(self):
+    def test_get_cart_success(self):
+        # test with invalid username, should return None
+        self.assertIsNone(self.store_facade.getCart("invalid_username"))
+
+        # test with valid username, should return a Cart object
+        cart = self.store_facade.getCart("JohnDoe")
+        self.assertIsInstance(cart, Cart)
+    def test_get_cart_failure(self):
         # test with invalid username, should return None
         self.assertIsNone(self.store_facade.getCart("invalid_username"))
 
@@ -164,66 +263,86 @@ class TestStoreFacade(TestCase):
         self.assertIsInstance(cart, Cart)
 
 
-    def test_addToBasket_valid_input(self):
+    def test_addToBasket_valid_input_success(self):
         pass
-
-    def test_addToBasket_invalid_quantity(self):
+    def test_addToBasket_valid_input_failure(self):
         pass
-
-    def test_addToBasket_invalid_product(self):
+    def test_addToBasket_invalid_quantity_success(self):
         pass
-
-    def test_addToBasket_invalid_store(self):
+    def test_addToBasket_invalid_quantity_failure(self):
         pass
-
-    def test_addToBasket_invalid_username(self):
+    def test_addToBasket_invalid_product_success(self):
         pass
-
-    def test_removeFromBasket_valid_input(self):
+    def test_addToBasket_invalid_product_failure(self):
         pass
-
-    def test_removeFromBasket_invalid_quantity(self):
+    def test_addToBasket_invalid_store_success(self):
         pass
-
-    def test_removeFromBasket_invalid_product(self):
+    def test_addToBasket_invalid_store_failure(self):
         pass
-
-    def test_removeFromBasket_invalid_store(self):
+    def test_addToBasket_invalid_username_success(self):
         pass
-
-    def test_removeFromBasket_invalid_username(self):
+    def test_addToBasket_invalid_username_failure(self):
         pass
-
-    def test_removeFromBasket_empty_cart(self):
+    def test_removeFromBasket_valid_input_success(self):
         pass
-    
-    def test_removeFromBasket_empty_basket(self):
+    def test_removeFromBasket_valid_input_failure(self):
         pass
-
-    def test_editBasketQuantity_valid_input(self):
+    def test_removeFromBasket_invalid_quantity_success(self):
         pass
-
-    def test_editBasketQuantity_invalid_quantity(self):
+    def test_removeFromBasket_invalid_quantity_failure(self):
         pass
-
-    def test_editBasketQuantity_invalid_product(self):
+    def test_removeFromBasket_invalid_product_success(self):
         pass
-
-    def test_editBasketQuantity_invalid_store(self):
+    def test_removeFromBasket_invalid_product_failure(self):
         pass
-
-    def test_editBasketQuantity_invalid_username(self):
+    def test_removeFromBasket_invalid_store_success(self):
         pass
-
-    def test_editBasketQuantity_empty_cart(self):
+    def test_removeFromBasket_invalid_store_failure(self):
         pass
-
-    def test_editBasketQuantity_empty_basket(self):
+    def test_removeFromBasket_invalid_username_success(self):
         pass
-
-    def test_purchaseCart_valid_input(self):
+    def test_removeFromBasket_invalid_username_failure(self):
         pass
-
+    def test_removeFromBasket_empty_cart_success(self):
+        pass
+    def test_removeFromBasket_empty_cart_failure(self):
+        pass
+    def test_removeFromBasket_empty_basket_success(self):
+        pass
+    def test_removeFromBasket_empty_basket_failure(self):
+        pass
+    def test_editBasketQuantity_valid_input_success(self):
+        pass
+    def test_editBasketQuantity_valid_input_failure(self):
+        pass
+    def test_editBasketQuantity_invalid_quantity_success(self):
+        pass
+    def test_editBasketQuantity_invalid_quantity_failure(self):
+        pass
+    def test_editBasketQuantity_invalid_product_success(self):
+        pass
+    def test_editBasketQuantity_invalid_product_failure(self):
+        pass
+    def test_editBasketQuantity_invalid_store_success(self):
+        pass
+    def test_editBasketQuantity_invalid_store_failure(self):
+        pass
+    def test_editBasketQuantity_invalid_username_success(self):
+        pass
+    def test_editBasketQuantity_invalid_username_failure(self):
+        pass
+    def test_editBasketQuantity_empty_cart_success(self):
+        pass
+    def test_editBasketQuantity_empty_cart_failure(self):
+        pass
+    def test_editBasketQuantity_empty_basket_success(self):
+        pass
+    def test_editBasketQuantity_empty_basket_failure(self):
+        pass
+    def test_purchaseCart_valid_input_success(self):
+        pass
+    def test_purchaseCart_valid_input_failure(self):
+        pass
 
 if __name__ == '__main__':
     unittest.main()
