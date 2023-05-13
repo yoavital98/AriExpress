bda0c7c650e421c060700dc35ecde18ceabd8bb1
feliks1 <feliks@post.bgu.ac.il>
2023-05-13 20:57:31 +0300

    for pull request

--- ProjectCode/Tests/test_concurrency.py
+++ ProjectCode/Tests/test_concurrency.py
@@ -7,19 +7,8 @@ from ProjectCode.Domain.MarketObjects.UserObjects.Admin import Admin
 from ProjectCode.Domain.MarketObjects.UserObjects.Member import Member
 from ProjectCode.Service.Service import Service
 import threading
-
-class TestStoreFacade(TestCase):
-
-    def setUp(self):
-        self.Service = Service()
-        # self.Service.register("username", "password", "email")
-        # self.Service.logIn("username", "password")
-        # self.Service.openStore("username", "storename")
-        # self.Service.addNewProductToStore("username", "storename", "product1", "category", 10, 10)
-        # self.Service.addNewProductToStore("username", "storename", "product2", "category", 10, 10)
-
-
-    def test_foo(func):
+    
+def test_func(func):
         # Create a shared variable to track the success/failure of foo()
         result = {'success_count': 0, 'failure_count': 0}
 
@@ -46,6 +35,19 @@ class TestStoreFacade(TestCase):
         assert result['success_count'] == 1, "One function call should succeed"
         assert result['failure_count'] == 1, "One function call should fail"
 
+class TestStoreFacade(TestCase):
+    def setUp(self):
+        self.Service = Service()
+        self.Service.register("username", "password", "email")
+        self.Service.logIn("username", "password")
+        self.Service.openStore("username", "storename")
+        self.Service.addNewProductToStore("username", "storename", "product1", "category", 10, 10)
+        self.Service.addNewProductToStore("username", "storename", "product2", "category", 10, 10)
+        self.Service.addToBasket("username", "storename", 1, 5)
+
+    def test_addToBasket_success(self):
+        test_func(self.Service.purchaseCart("username", 1234, "feliks", 123456789, 1, 111, "okokok"))
+    
 
 
 
