0bf8967e28bcc12cd9ff0a13c48d84fbb4799a7f
Ari <arioshryz@gmail.com>
2023-06-10 08:35:39 +0300

    integration tests

--- ProjectCode/Tests/test_integration.py
+++ ProjectCode/Tests/test_integration.py
@@ -83,26 +83,21 @@ class Test_Use_Cases_2_1(TestCase):
 class Test_Use_Case_2_1_4(TestCase):
 
     def setUp(self):
-        self.Service = Service()
-        self.Service.register("username", "password", "email")
+        self.service = Service()
 
 
     def test_login_to_the_system_success(self):
 
-        res = self.Service.logIn("username", "password")
+        res = self.service.loginAsGuest()
         self.assertTrue(res.getStatus())
-        return_value = res.getReturnValue()
-        self.assertTrue(return_value.get('email') == 'email')
-        self.assertTrue(return_value.get('entrance_id') == '0')
-        self.assertTrue(return_value.get('username') == 'username')
 
     def test_logging_in_the_system_failure(self):
-        res = self.Service.logIn("username", "passwo")
-        self.assertIsInstance(res.getReturnValue(), Exception, "The system shouldn't log in")
+        # there is no way to fail this
+        pass
 
 
     #Use Case 2.2.1
-class Test_Use_Case_2_2(TestCase):
+class Test_Use_Case_2_guests(TestCase):
     def setUp(self):
         self.service = Service()
         self.service.register("Feliks", "password456", "feliks@gmail.com")
@@ -208,6 +203,69 @@ class Test_Use_Case_2_2(TestCase):
                                        "be'er sheva")
         self.assertTrue(res_purchase.getStatus())
 
+    # Use Case 2.2.5.b,c guests cant participate in bids
+    def test_guest_purchaseConfirmedBid_success(self):
+        pass
+        # todo: ver 3
+
+
+
+class Test_Use_Case_3_members(TestCase):
+    def setUp(self):
+        self.service = Service()
+        self.service.register("username", "password", "email")
+
+    # Use Case 3.1.1
+    def test_login_to_the_system_success(self):
+        res = self.service.logIn("username", "password")
+        self.assertTrue(res.getStatus())
+        return_value = res.getReturnValue()
+        self.assertTrue(return_value.get('email') == 'email')
+        self.assertTrue(return_value.get('entrance_id') == '0')
+        self.assertTrue(return_value.get('username') == 'username')
+
+    def test_logging_in_the_system_failure(self):
+        res = self.service.logIn("username", "passwo")
+        self.assertIsInstance(res.getReturnValue(), Exception, "The system shouldn't log in")
+
+    # Use Case 3.1.2
+    def test_log_out_from_the_system_success(self):
+        res = self.service.logIn("username", "password")
+        self.assertTrue(res.getStatus())
+        res_logout = self.service.logOut("username")
+        self.assertTrue(res_logout.getStatus())
+
+    def test_log_out_from_the_system_fail(self):
+        res = self.service.logIn("username", "password")
+        self.assertTrue(res.getStatus())
+        res_logout = self.service.logOut("userna")
+        self.assertFalse(res_logout.getStatus())
+
+    # Use Case 3.2
+    def test_createShop_Success(self):
+        res = self.service.logIn("username", "password")
+        self.assertTrue(res.getStatus())
+        res_create_store = self.service.createStore("username", "AriExpress")
+        self.assertTrue(res_create_store.getStatus())
+
+    class Test_Use_Case_4_Management(TestCase):
+        def setUp(self):
+            self.service = Service()
+            self.service.register("Feliks", "password456", "feliks@gmail.com")
+            self.service.register("Amiel", "password789", "amiel@gmail.com")
+            self.service.logIn("Feliks", "password456")
+            self.service.createStore("Feliks", "AriExpress")
+            self.service.logOut("Feliks")
+
+
+
+
+# Use Case 3.2
+    def test_addProductToStore_Success(self):
+        res = self.service.logIn("Feliks", "password456")
+        self.assertTrue(res.getStatus())
+        res_new_product = self.service.addNewProductToStore("Feliks","AriExpress", "paper", "paper", 50, 100)
+        self.assertTrue(res_new_product.getStatus())
 
 
 if __name__ == '__main__':
