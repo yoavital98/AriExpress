a6b211bed434f7c7c7e047ba2a5d493fadea2a12
Amiel <amielsaa@post.bgu.ac.il>
2023-06-21 01:41:06 +0300

    added small fix

--- ProjectCode/DAL/NotificationModel.py
+++ ProjectCode/DAL/NotificationModel.py
@@ -10,9 +10,9 @@ class NotificationModel(Model):
         db_table = 'notification'
 
     notification_id = AutoField()
-    sender_id = CharField(max_length=100)
-    receiver_id = CharField(max_length=100)
-    subject = CharField(max_length=100)
-    content = CharField(max_length=100)
+    sender_id = CharField(max_length=1000)
+    receiver_id = CharField(max_length=1000)
+    subject = CharField(max_length=1000)
+    content = CharField(max_length=1000)
     date = DateTimeField()
     read = BooleanField(default=False)
